16d7409f0d8c11b9e8e3fe541168e79011d20d28
Leandro Lucas dos Santos <leandrosantos182@gmail.com>
2021-07-16 19:17:43 -0300

    novo Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+} /*mostrar menu*/
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+} /*esconder menu*/
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
