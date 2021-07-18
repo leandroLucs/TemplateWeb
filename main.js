b93002a3991d56f83ad998f00a04fbc45f35735a
Leandro Lucas dos Santos <leandrosantos182@gmail.com>
2021-07-18 19:45:30 -0300

    adicionando swiper e scrollreveal para estilização da página e finalizando as abas de depoímentos e contatos

--- main.js
+++ main.js
@@ -20,3 +20,29 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+/*Biblioteca swiper para slider, carousel no TESTIMONIALS*/
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+/*Biblioteca Scroll Reveal*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
