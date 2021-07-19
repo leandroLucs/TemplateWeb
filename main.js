74eafd29abe9b277b2d44344bc7095d227d627a0
Leandro Lucas dos Santos <leandrosantos182@gmail.com>
2021-07-19 13:59:12 -0300

    Adicionando responsividade para telas >1200px

--- main.js
+++ main.js
@@ -20,7 +20,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 /*Biblioteca Scroll Reveal*/
 const scrollReveal = ScrollReveal({
@@ -41,9 +47,9 @@ scrollReveal.reveal(
 )
 
 /*mudar header*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -52,8 +58,8 @@ function changeHeaderWhenScroll() {
 }
 
 /*botão voltar para o topo*/
+const backTopBtn = document.querySelector('.back-to-top')
 function backToTop() {
-  const backTopBtn = document.querySelector('.back-to-top')
   if (window.scrollY >= 560) {
     backTopBtn.classList.add('show')
   } else {
@@ -61,8 +67,31 @@ function backToTop() {
   }
 }
 
+/*menu ativo conforme seção visivel*/
+const sections = document.querySelectorAll('main section[id]')
+function currentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /*when scroll*/
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  currentSection()
 })
