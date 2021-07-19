df1c8ec9a8de3db56f131feb4d60e7fcfa80d65e
Leandro Lucas dos Santos <leandrosantos182@gmail.com>
2021-07-19 11:07:25 -0300

    Conclusão das seções depoimentos e contato, adição de um Footer e transições básicas da página

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const links = document.querySelectorAll('nav ul li a')
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
@@ -13,13 +12,7 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 } /*esconder menu*/
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-})
+
 /*Biblioteca swiper para slider, carousel no TESTIMONIALS*/
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -36,13 +29,40 @@ const scrollReveal = ScrollReveal({
   duration: 700,
   reset: true
 })
-
 scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
   #contact .text, #contact .links
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+/*mudar header*/
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/*botão voltar para o topo*/
+function backToTop() {
+  const backTopBtn = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backTopBtn.classList.add('show')
+  } else {
+    backTopBtn.classList.remove('show')
+  }
+}
+
+/*when scroll*/
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
